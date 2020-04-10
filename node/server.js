b39687640a523ea1a49957023bc5ac61077e57d5
Zoe Heins <zoe.heins@thinkful.com>
2020-04-10 13:22:05 -0700

    boilerplate

---
+++ node/server.js
@@ -0,0 +1,35 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+// load environment variables
+// TODO: check environment before loading ENV
+require('dotenv').config()
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
+
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/movieDatabase', function(err, client) {
+  if (err) throw err;
+  const db = client.db('movieDatabase');
+  const movieCollection = db.collection('movies');
+
+  console.log(process.env.API_TOKEN)
+
+  server.get('/', function (req, res) {
+    movieCollection.find().toArray(function (err, result) {
+      if (err) throw err;
+      const movies = result;
+      res.send(movies);
+    });
+  });
+});
+
+server.listen(8000, function () {
+  console.log('listening.........');
+});
+
+module.exports = server;
---
+++ web/src/App.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+    this.fetchMovies();
+  }
+
+  fetchMovies = () => {
+    axios.get('http://localhost:8000/').then((res) => {
+      const movies = res.data;
+      this.setState({ movies });
+    });
+  };
+
+  render() {
+    const { movies } = this.state;
+    console.log(movies);
+    return (
+      <div>
+        {movies.map((movie) => (
+          <p key={movie.title}>{movie.title}</p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
